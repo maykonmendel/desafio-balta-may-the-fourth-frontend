f60d53a8049aa5040edc7c2ab8cf1ff84fff47a9
Maykon Mendel <maykonmendel.dev@gmail.com>
2024-04-29 20:35:54 -0300

    Implementa serviço de Filmes

---
+++ MayTheFourth/Services/MovieService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Net.Http.Json;
+using MayTheFourth.Models;
+
+namespace MayTheFourth.Services;
+
+public class MovieService(IHttpClientFactory httpClientFactory)
+{
+    public async Task<IEnumerable<Movie>> GetMoviesAsync()
+    {
+        var client = httpClientFactory.CreateClient(Configuration.HttpClientName);
+        return await client.GetFromJsonAsync<IEnumerable<Movie>>("api/filmes") ?? [];
+    }
+}
